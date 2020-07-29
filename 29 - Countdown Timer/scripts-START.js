25d2645c4bd5bb0df9707e5528844df1ddc4532a
Joanna Huang <joannathhuang@gmail.com>
2020-07-29 17:24:54 +0800

    Day 29 in progress

---
+++ 29 - Countdown Timer/scripts-START.js
@@ -0,0 +1,41 @@
+let countdown;
+let timerDisplay = document.querySelector(".display__time-left");
+let endTime = document.querySelector(".display__end-time");
+
+function timer(seconds) {
+  const now = Date.now();
+  const then = now + seconds * 1000;
+  displayTimeLeft(seconds);
+  displayEndTime(then);
+
+  countdown = setInterval(() => {
+    const secondsLeft = Math.round((then - Date.now()) / 1000);
+
+    if (secondsLeft < 0) {
+      clearInterval(countdown);
+      return;
+    }
+    displayTimeLeft(secondsLeft);
+  }, 1000);
+}
+
+function displayTimeLeft(seconds) {
+  const minutes = Math.floor(seconds / 60);
+  const remainderSeconds = seconds % 60;
+  const display = `${minutes}:${
+    remainderSeconds < 10 ? `0` : ``
+  }${remainderSeconds}`;
+  timerDisplay.textContent = display;
+  document.title = display;
+  console.log(minutes, remainderSeconds);
+}
+
+function displayEndTime(timestamp) {
+  const end = new Date(timestamp);
+  const hour = end.getHours();
+  const adjustedHour = hour > 12 ? hour - 12 : hour;
+  const minutes = end.getMinutes();
+  endTime.textContent = `Be back at ${adjustedHour}:${
+    minutes < 10 ? "0" : ""
+  }${minutes}`;
+}
